8509e17b24ccd597dc8045fce06eaf925e3954db
rock-n-shrimproll <alexandra.dolidze@gmail.com>
2022-03-31 09:52:37 +0300

    Вторая домашка

--- hw2_2.hs
+++ hw2_2.hs
@@ -1,9 +1,17 @@
 module Fun where
+
   newtype Fun a b = Fun {getFun :: a -> b}
 
   instance Functor (Fun a) where
-  fmap =
+    fmap f (Fun b) = Fun(\x -> f (b x))             
+
+    -- instance Functor ((->) a) where
+    -- fmap f g = f . g
 
   instance Applicative (Fun a) where
-  pure =
-  (<*>) =  
+    pure a = Fun(\_ -> a)
+    Fun a <*> Fun b = Fun(\x -> (a x) (b x))
+
+    -- instance Applicative ((->) a) where
+    -- pure x = (\_ -> x)
+    -- f <*> g = \x -> f x (g x)
